d7ffb9479d75a051991b9f41070ebb06bda06c96
Joan Capell, Gràcia <joanc1997@hotmail.com>
2018-11-27 11:54:34 +0100

    AI

--- AI.pl
+++ AI.pl
@@ -1,6 +1,7 @@
 %Choose best move from the actual position searching Depth turns
 %BestMove is the best move in the form (move, value)
 %Record is the best move until now in the form (move, value)
+%TODO: Test
 evaluate_and_choose([Move|Moves], Player, Board, Depth, MaxMin, Record, BestMove):-
     playMove(Board,Move,NewBoard,Player),
     changePlayer(Player, NewPlayer),
@@ -14,23 +15,43 @@ minimax(0, Board, Player, MaxMin, Move, Value):-
     value(Board, ValueBoard),
     Value is ValueBoard*MaxMin.
 
+
+
 minimax(Depth, Board, Player, MaxMin, Move, Value) :-
     Depth > 0,
     %TODO: realation move
-    set_of(M, possible_move(Board, M), Moves),
+    setof(M, possible_move(Board, M), Moves),
     NewDepth is Depth - 1,
     MinMax is -MaxMin,
     evaluate_and_choose(Moves, Board, NewDepth, MinMax, (nil, -1000),
                         (Move, Value)).
 update(Move, Value, (Move1, Value1), (Move1, Value1)):-
-    Value <= Value1.
+    Value =< Value1.
 
 update(Move, Value, (Move1, Value1), (Move, Value)) :-
-    Value > Value1.
+	Value > Value1.
 
-%Define the heuristic
+%TODO: Define the heuristic
+%x is positive, o is negative
 value(Board, 1).
 
+%TODO: move to other file
+possible_move(Board, Move) :-
+	validColumn(Move),
+	not(isColFull(nth0(0,Board,Col0))).
+
+validColumn(0).
+validColumn(1).
+validColumn(2).
+validColumn(3).
+validColumn(4).
+validColumn(5).
+validColumn(6).
+
+%TODO: use the one in the other file
+isColFull(A):-
+	fail,
+	!.
 
 
 
