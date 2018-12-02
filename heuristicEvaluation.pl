01e3a053fbc560913f30cfc6242e1a83c4ca0419
ratatouille <7spherebleue7@gmail.com>
2018-12-02 15:39:29 +0100

    add sum of value and value(Board,Value)

--- heuristicEvaluation.pl
+++ heuristicEvaluation.pl
@@ -80,3 +80,44 @@ createListFromDiagonalDown([C|B], [Head|ReturnQ], 1, LineIndex) :- append(_,[Hea
 createListFromDiagonalDown([_|B], [Head|ReturnQ], ColumnIndex, LineIndex) :- NewColumnIndex is ColumnIndex - 1,
     createListFromDiagonalDown(B, [Head|ReturnQ], NewColumnIndex, LineIndex).
 	
+
+
+%%%%%% Value should be initialized to 0. [C|Q] is the Board. Final is the return value.
+valueColumn([],Value, Final) :- Final is Value.
+valueColumn([C|Q], Value, Final) :- evaluateList(C, 'x', 0, 0, 0, ToAdd), evaluateList(C, 'o', 0, 0, 0, ToSub),
+    ValColumn is Value + ToAdd - ToSub, valueColumn(Q, ValColumn, Final).
+
+%%%%%% Index should be initialized to 1, Value should be initialized to 0. Return Final.
+valueLine([CBoard|_], Index, Value, Final) :- length(CBoard,Lgt), Index is Lgt+1, Final is Value.
+valueLine([CBoard|QBoard], Index, Value, Final) :- createListWithLine([CBoard|QBoard], C, Index), 
+    evaluateList(C, 'x', 0, 0, 0, ToAdd), evaluateList(C, 'o', 0, 0, 0, ToSub),
+    ValLine is Value + ToAdd - ToSub, NewIndex is Index+1, valueLine([CBoard|QBoard], NewIndex, ValLine, Final).
+
+%%%%%% IndexL should be initialized to 1, IndexC should be initialized to 1, 
+% Value should be initialized to 0. Return Final.
+valueDiagonalUp([CBoard|QBoard], IndexC, _, Value, Final) :- length([CBoard|QBoard], Ht), IndexC is Ht+1,
+    Final is Value.
+valueDiagonalUp([CBoard|QBoard], IndexC, IndexL, Value, Final) :- length([CBoard|QBoard], Ht), Ht+1 > IndexC,
+    length(CBoard,Lgt), IndexL is Lgt+1, NewIndexC is IndexC +1, 
+    valueDiagonalUp([CBoard|QBoard], NewIndexC, 1, Value, Final).
+valueDiagonalUp([CBoard|QBoard], IndexC, IndexL, Value, Final) :- 
+    createListWithDiagonaleUp([CBoard|QBoard], C, IndexC, IndexL), 
+    evaluateList(C, 'x', 0, 0, 0, ToAdd), evaluateList(C, 'o', 0, 0, 0, ToSub),
+    ValLine is Value + ToAdd - ToSub, NewIndexL is IndexL+1, 
+    valueDiagonalUp([CBoard|QBoard], IndexC, NewIndexL, ValLine, Final).
+
+%%%%%% IndexL should be initialized to 1, IndexC should be initialized to 1, 
+% Value should be initialized to 0. Return Final.
+valueDiagonalDown([CBoard|QBoard], IndexC, _, Value, Final) :- length([CBoard|QBoard], Ht), IndexC is Ht+1,
+    Final is Value.
+valueDiagonalDown([CBoard|QBoard], IndexC, IndexL, Value, Final) :- length([CBoard|QBoard], Ht), Ht+1 > IndexC,
+    length(CBoard,Lgt), IndexL is Lgt+1, NewIndexC is IndexC +1, 
+    valueDiagonalDown([CBoard|QBoard], NewIndexC, 1, Value, Final).
+valueDiagonalDown([CBoard|QBoard], IndexC, IndexL, Value, Final) :- 
+    createListWithDiagonaleDown([CBoard|QBoard], C, IndexC, IndexL), 
+    evaluateList(C, 'x', 0, 0, 0, ToAdd), evaluateList(C, 'o', 0, 0, 0, ToSub),
+    ValLine is Value + ToAdd - ToSub, NewIndexL is IndexL+1, 
+    valueDiagonalDown([CBoard|QBoard], IndexC, NewIndexL, ValLine, Final).
+
+value(Board, Value) :- valueDiagonalDown(Board, 1, 1, 0, Final1), valueDiagonalUp(Board, 1, 1, 0, Final2),
+    valueColumn(Board, 0, Final3), valueLine(Board, 1, 0, Final4), Value is Final1 + Final2 + Final3 + Final4.
